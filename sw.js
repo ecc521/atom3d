5bc23d144d652d9cb3f9dae9877d5934dfb0583d
ecc521 <42424091+ecc521@users.noreply.github.com>
2020-09-16 12:01:39 -0400

    Upgrade serviceworker to network with cache fallback.

--- sw.js
+++ sw.js
@@ -1,3 +1,19 @@
+
 self.addEventListener('fetch', function(event) {
-	return; //No-op for now, due to PWA requirements.
+	let promise = fetch(event.request)
+	let cacheLoader = caches.open('atombohrmodels3d')
+	promise.then((response) => {
+		let resp2 = response.clone()
+		cacheLoader.then((cache) => {
+			console.log("Writing into cache " + event.request.url)
+			cache.put(event.request, resp2);
+		})
+		return response
+	})
+	promise.catch((response) => {
+		console.log("Using cache as fallback for " + event.request.url)
+		return caches.match(event.request)
+	})
+
+  event.respondWith(promise);
 });
