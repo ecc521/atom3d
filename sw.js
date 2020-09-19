c4577a75c89923280dcbaa24e3eef7c83a222737
ecc521 <42424091+ecc521@users.noreply.github.com>
2020-09-19 11:25:58 -0400

    Rewrite serviceworker, fixes a bug that prevented cache from working.

--- sw.js
+++ sw.js
@@ -1,19 +1,26 @@
 
 self.addEventListener('fetch', function(event) {
-	let promise = fetch(event.request)
+	//let promise = fetch(event.request)
 	let cacheLoader = caches.open('atombohrmodels3d')
-	promise.then((response) => {
-		let resp2 = response.clone()
-		cacheLoader.then((cache) => {
-			console.log("Writing into cache " + event.request.url)
-			cache.put(event.request, resp2);
-		})
-		return response
-	})
-	promise.catch((response) => {
-		console.log("Using cache as fallback for " + event.request.url)
-		return caches.match(event.request)
-	})
-
-  event.respondWith(promise);
+	event.respondWith((async function() {
+		let response, cache;
+		try {
+			response = await fetch(event.request)
+			let clone = response.clone()
+			cache = await cacheLoader
+			cache.put(event.request, clone);
+			console.log("From network: " + event.request.url)
+			return await response
+		}
+		catch (e) {
+			//Return from cache.
+			cache = await cacheLoader
+			console.log("From cache: " + event.request.url)
+			return await cache.match(event.request)
+		}
+	})())
 });
+
+self.addEventListener("install", function() {
+    self.skipWaiting()
+})
